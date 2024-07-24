5790c592a37165be459f15b56ca9f2ddf55f37f4
John Miko Barreyro <barreyrojohnmiko@gmail.com>
2024-07-24 20:54:59 +0800

    added baseName

--- src/router.jsx
+++ src/router.jsx
@@ -1,18 +1,22 @@
 import { createBrowserRouter } from 'react-router-dom';
 
+import LandingPageComponent from './components/LandingPage/page';
 import NotFound from './errors/NotFound';
 
-import LandingPageComponent from './components/LandingPage/page';
-
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <LandingPageComponent />,
+    },
+    {
+      path: '*',
+      element: <NotFound />,
+    },
+  ],
   {
-    path: '/',
-    element: <LandingPageComponent />,
-  },
-  {
-    path: '*',
-    element: <NotFound />,
-  },
-]);
+    basename: '/SKUvantageUI/',
+  }
+);
 
 export default router;
